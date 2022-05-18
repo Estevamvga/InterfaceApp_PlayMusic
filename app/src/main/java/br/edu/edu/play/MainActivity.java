5d219d0605d5079756fb43c13e4be03e9199e9f7
Estevamvga <estevam.pereira@outlook.com>
2022-05-17 23:42:19 -0300

    Commit intial play music

---
+++ app/src/main/java/br/edu/edu/play/MainActivity.java
@@ -0,0 +1,14 @@
+package br.edu.edu.play;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
